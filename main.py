d60a5435cfd9c14b41be147d9a1d9fea4c372ea0
Alexqq11 <alt.alexqq11@gmail.com>
2023-05-25 01:19:21 +0500

    try with post and bd

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
+#app.py
+from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
+db = SQLAlchemy(app)
 
-@app.route('/hello')
-def hello():
-   return 'Hello, World!'
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    position = db.Column(db.String(50))
+
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+        
+db.create_all()
+
+@app.route('/add_employee', methods=['POST'])
+def add_employee():
+    name = request.form['name']
+    position = request.form['position']
+    employee = Employee(name=name, position=position)
+    db.session.add(employee)
+    db.session.commit()
+    return {"success":'Employee added successfully'}
+
+@app.route('/get_employee/<int:id>')
+def get_employee(id):
+    employee = Employee.query.get(id)
+    if employee:
+        return jsonify({
+            'id': employee.id,
+            'name': employee.name,
+            'position': employee.position
+        })
+    else:
+        return {'error': 'Employee not found'}
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app.run(host = "0.0.0.0", debug=True)
