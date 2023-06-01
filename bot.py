a3f5656a6c3e73bfa2383d33fbffc6ef20933846
Alexqq11 <alt.alexqq11@gmail.com>
2023-06-01 20:41:38 +0500

    added bot

--- bot.py
+++ bot.py
@@ -5,25 +5,26 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 TELEGRAM_TOKEN = "6057583176:AAE1tbvoX0ID5_1R8SMZT9dafwzpqZ9tXUY"
 API_URL = "http://45.131.40.79:5000"
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+#logging.basicConfig(
+#    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#    level=logging.INFO
+#)
 
 
 def get_employees():
     response = requests.get(f"{API_URL}/employees")
     if response:
         employees = response.json()
-        return "\n".join([f"{employee['name']}, {employee['position']}" for employee in employees])
+        return "\n".join([f"{employee['id']}, {employee['name']}, {employee['position']}" for employee in employees])
 
 
 def get_employee(id):
-    url = f"{API_URL}/delete_employee/{id}"
+    url = f"{API_URL}/get_employee/{id}"
     response = requests.get(url)
     if response:
         employee = response.json()
-        return f"{employee['name']}, {employee['position']}"
+        if not "error" in employee:
+            return f"{employee['name']}, {employee['position']}"
 
 
 def add_employee(name, position):
@@ -54,10 +55,95 @@ async def start(update, context):
                                    text='Welcome to Employee Bot! Use /employees to get a list of all employees.')
 
 
+async def handle_employees(update, context, args):
+    message_body = get_employees()
+    if message_body:
+        await context.bot.send_message(chat_id=update.message.chat.id, text=message_body)
+    else:
+        await context.bot.send_message(chat_id=update.message.chat.id,
+                                       text='Sorry, something went wrong. Please try again later.')
+
+
+async def handle_get_employee(update, context, args):
+    if len(args) == 1:
+        id = args[0]
+        msg = get_employee(id)
+        if msg:
+            await context.bot.send_message(chat_id=update.message.chat.id, text=msg)
+        else:
+            await context.bot.send_message(chat_id=update.message.chat.id,
+                                           text='Sorry, something went wrong. Please try again later.')
+
+
+
+async def handle_add_employees(update, context, args):
+    if len(args) == 2:
+        name, position = args
+        if add_employee(name, position):
+            await context.bot.send_message(chat_id=update.message.chat.id, text='Employee added successfully.')
+        else:
+            await context.bot.send_message(chat_id=update.message.chat.id,
+                                         text='Sorry, something went wrong. Please try again later.')
+    else:
+        await context.bot.send_message(chat_id=update.message.chat.id, text='Invalid number of arguments.')
+
+
+async def handle_update_employees(update, context, args):
+    if len(args) == 3:
+        id, name, position = args
+        if update_employee(id, name, position):
+            await context.bot.send_message(chat_id=update.message.chat.id, text='Employee updated successfully.')
+        else:
+            await context.bot.send_message(chat_id=update.message.chat.id,
+                                           text='Sorry, something went wrong. Please try again later.')
+    else:
+        await context.bot.send_message(chat_id=update.message.chat.id, text='Invalid number of arguments.')
+
+
+async def handle_delete_employees(update, context, args):
+    if len(args) == 1:
+        id = args[0]
+        if delete_employee(id):
+            await context.bot.send_message(chat_id=update.message.chat.id, text='Employee deleted successfully.')
+        else:
+            await context.bot.send_message(chat_id=update.message.chat.id,
+                                           text='Sorry, something went wrong. Please try again later.')
+
+async def handle_request(update, context):
+        text = update.message.text.lower().split(' ')
+        command, args = text[0], text[1:]
+
+        if command == '/employees':
+            await handle_employees(update, context, args)
+            pass
+        elif command == '/get_employee':
+            await handle_get_employee(update, context, args)
+            pass
+        elif command == '/add_employee':
+            await handle_add_employees(update, context, args)
+            pass
+        elif command == '/update_employee':
+            await handle_update_employees(update, context, args)
+            pass
+        elif command == '/delete_employee':
+            await handle_delete_employees(update, context, args)
+            pass
+        else:
+            await context.bot.send_message(chat_id=update.message.chat.id, text='Invalid command.')
+
+
+
+
+
+
 def main():
     application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    api_handler = MessageHandler(filters.TEXT, handle_request)
+    application.add_handler(api_handler)
+
+
     application.run_polling()
 
 if __name__ == '__main__':
